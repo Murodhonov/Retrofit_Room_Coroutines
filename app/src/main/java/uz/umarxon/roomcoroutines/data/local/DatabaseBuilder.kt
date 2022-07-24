4410ac2bee8891d69a366993889aaf2a10c86e4f
Umarxon <86461164+Murodhonov@users.noreply.github.com>
2022-07-24 13:15:19 +0500

    Update DatabaseBuilder.kt

--- app/src/main/java/uz/umarxon/roomcoroutines/data/local/DatabaseBuilder.kt
+++ app/src/main/java/uz/umarxon/roomcoroutines/data/local/DatabaseBuilder.kt
@@ -20,7 +20,7 @@ object DatabaseBuilder {
         Room.databaseBuilder(
             context.applicationContext,
             AppDatabase::class.java,
-            "mindorks-example-coroutines"
+            "murodhonovuz-example-coroutines"
         ).build()
 
 }
